be51df30b3c65b96b9bb5a4bfe8c8eb6da1e22f4
Kavi Mookherjee Amodt <kmook@berkeley.edu>
2026-07-18 20:16:46 +0000

    now finished database migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'Aladdin', :rating => 'G',
+    :release_date => '25-Nov-1992'},
+  {:title => 'When Harry Met Sally', :rating => 'R',
+    :release_date => '21-Jul-1989'},
+  {:title => 'The Help', :rating => 'PG-13',
+    :release_date => '10-Aug-2011'},
+  {:title => 'Raiders of the Lost Ark', :rating => 'PG',
+    :release_date => '12-Jun-1981'}
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
